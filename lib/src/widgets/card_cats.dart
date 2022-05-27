2947aa058fc0889ebf8cfcb8a948a79dbdf0c98d
yderfre78 <72670647+yderfre78@users.noreply.github.com>
2022-05-27 17:42:42 -0500

    create navigation card detail

--- lib/src/widgets/card_cats.dart
+++ lib/src/widgets/card_cats.dart
@@ -7,26 +7,43 @@ class CardCats extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Container(
-      width: double.infinity,
-      height: size.height * 0.8,
-      color: Colors.red,
-      child: Swiper(
-        itemCount: 10,
-      
-        scrollDirection: Axis.vertical,
-          pagination: const SwiperPagination(alignment: Alignment.centerRight),
-          control: const SwiperControl(),
-        layout: SwiperLayout.STACK,
-        itemWidth: size.width * 1,
-        itemHeight: size.height *1,
-        itemBuilder: (__, int index) {
-          return FadeInImage(
-            placeholder: NetworkImage('https://via.placeholder.com/300x400'),
-            image: NetworkImage('https://via.placeholder.com/300x400'),
-            fit: BoxFit.fill,
-          );
-        },
+    return ClipRrect(size: size);
+  }
+}
+
+class ClipRrect extends StatelessWidget {
+  const ClipRrect({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: ()=> Navigator.pushNamed(context, 'details', arguments: 'mopvie-instance'),
+      child: Container(
+        width: double.infinity,
+        height: size.height * 0.8,
+        color: Colors.red,
+        child: Swiper(
+          itemCount: 10,
+        
+          scrollDirection: Axis.vertical,
+            pagination: const SwiperPagination(alignment: Alignment.centerRight),
+            control: const SwiperControl(),
+          layout: SwiperLayout.STACK,
+          itemWidth: size.width * 1,
+          itemHeight: size.height *1,
+          itemBuilder: (__, int index) {
+            return const FadeInImage(
+              placeholder: AssetImage('assets/images/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/300x400'),
+              fit: BoxFit.fill,
+            );
+          },
+        ),
       ),
     );
   }
