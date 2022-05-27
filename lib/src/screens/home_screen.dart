3a5ab3acebd4f7358b1e978b71cdf581e548a20f
yderfre78 <72670647+yderfre78@users.noreply.github.com>
2022-05-26 22:18:28 -0500

    create pages home detail

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 
-
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-     ),
-   );
+    return Container(
+      child: Text('HomeScreen'),
+    );
   }
 }
